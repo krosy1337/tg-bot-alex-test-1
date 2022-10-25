cb720ee4fd565d1919ca9d0970b8bcdc7c24f749
Alexey Egorov <krosy1337@gmail.com>
2022-10-25 19:18:35 +0300

    what?

--- index.js
+++ index.js
@@ -1,54 +1,85 @@
 const TelegramApi = require("node-telegram-bot-api")
-const {gameOptions, againOptions} = require("./options")
+const express = require("express")
+const cors = require("cors")
+
 
 const TOKEN = "5684851368:AAHJ7FXepgYoQZnxFnlmyaHexxhdcbAqke8"
 
-const bot = new TelegramApi(TOKEN, {polling: true})
+const webAppUrl = "https://tg-bot-alex-test-1-web-app.vercel.app"
 
+const bot = new TelegramApi(TOKEN, {polling: true})
+const app = express()
+
+
+app.use(express.json())
+app.use(cors())
 
 const chats = {}
 
 const startGame = async chatId => {
     const randomDigit = Math.floor(Math.random()*10)
     chats[chatId] = randomDigit
-    await bot.sendMessage(chatId, "Я загадал число от 1 до 9, попробуй его отгадать", gameOptions)
+    botLastMsg = await bot.sendMessage(chatId, "Я загадал число от 1 до 9, попробуй его отгадать", gameOptions)
 }
 
-const start = () => {
-    bot.setMyCommands([
-        {command: "/start", description: "Старт"},
-        {command: "/info", description: "Инфо эбаут ботик"},
-        {command: "/game", description: "Начать игру: \"Угадай цифру\""},
-    ])
+let botLastMsg = null
 
+const start = () => {
     bot.on("message", async msg => {
         const text = msg.text
         const chatId = msg.chat.id
         if (text === "/start") {
-            return bot.sendSticker(chatId, "CAACAgIAAxkBAAEGD1ZjRVnrlaHr2-zua7C3Df3GiISS7wACGyIAAgtu6EkU-a0BH7uXiCoE")
+            await bot.sendMessage(chatId, "Сделай заказец в нашем магазе", {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{text: "Заказец", web_app: {url: webAppUrl}}]
+                    ],
+                }
+            })
+            await bot.sendMessage(chatId, "Ниже появится кнопка, заполни форму", {
+                reply_markup: {
+                    keyboard: [
+                        [{text: "Заполнить форму", web_app: {url: webAppUrl + "/form"}}]
+                    ],
+                }
+            })
         }
-        if (text === "/info") {
-            return bot.sendMessage(chatId, "Ama bot")
-        }
-        if (text === "/game") {
-            return startGame(chatId)
-        }
-        return bot.sendMessage(chatId, "IDK")
-    })
-    bot.on("callback_query", async msg => {
-        const data = msg.data
-        const chatId = msg.message.chat.id
-        console.log(data, chats[chatId])
-        if (data === "/again") {
-            return startGame(chatId)
-        }
-        if (parseInt(data) === chats[chatId]) {
-            await bot.sendMessage(chatId, "Ты угадал, поздравляю")
-            return bot.sendSticker(chatId, "CAACAgIAAxkBAAEGD2tjRWHqnw9p-Yu4AAGS_hIPLX2SOnAAAiAgAAJ1zeFJVRXAe8v35RMqBA", againOptions)
-        } else {
-            return bot.sendMessage(chatId, `К сожалению ты не угадал, бот загадал число ${chats[chatId]}`, againOptions)
+
+        if (msg.web_app_data?.data) {
+            try {
+                const data = JSON.parse(msg.web_app_data?.data)
+                console.log(data)
+                await bot.sendMessage(chatId, "Спасибо за обратную связь")
+                await bot.sendMessage(chatId, "Ваша страна " + data.country)
+            }
+            catch (e) {
+                console.log(e)
+            }
         }
     })
 }
 
 start()
+
+app.post("/web-data", async (req, res) => {
+    const {queryId, products, totalPrice} = req.body
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: "article",
+            id: queryId,
+            title: "Заказ успешно сделан",
+            input_message_content: {
+                message_text: "Конгратюлэйшинс, вы потратили " + totalPrice + " деревянных"
+            }
+        })
+        return res.status(200).json({})
+    }
+    catch (e) {
+        console.log(e)
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => console.log("Сервак дал газу на порту " + PORT))
